5b8859c76819589e93c6a36c84a189b27d43f5cd
JdbermeoUZH <juandiego.bermeoortiz@uzh.ch>
2022-11-04 10:26:35 +0100

    added gt to broken up 2D volume

--- code/dataset/breakup_volume_into_2D.py
+++ code/dataset/breakup_volume_into_2D.py
@@ -7,34 +7,57 @@ import nibabel as nib
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--volume_path', type=str)
+    parser.add_argument('--blurred_volume_path', type=str)
+    parser.add_argument('--gt_volume_dir', type=str)
     parser.add_argument('--output_2D_images_dir', type=str)
     parser.add_argument('--save_every_n_cuts', type=int, default=8)
     args = parser.parse_args()
 
-    # Load volume
-    img = nib.load(args.volume_path).get_fdata()
+    # Load blurred volume and ground truth
+    img_gt = nib.load(os.path.join(args.gt_volume_dir, os.path.basename(args.blurred_volume_path))).get_fdata()
+    img_blurred = nib.load(args.blurred_volume_path).get_fdata()
 
     # Loop over each of the images and break it into 2D images
-    os.makedirs(args.output_2D_images_dir, exist_ok=True)
+    gt_dir = os.path.join(args.output_2D_images_dir, "ground_truth")
+    blured_dir = os.path.join(args.output_2D_images_dir, "blurred")
+    os.makedirs(gt_dir, exist_ok=True)
+    os.makedirs(blured_dir, exist_ok=True)
 
-    for depth_x in range(0, img.shape[0], args.save_every_n_cuts):
-        im = Image.fromarray(img[depth_x, :, :])
-        im.convert('RGB').save(os.path.join(
-            args.output_2D_images_dir,
-            f"{os.path.basename(args.volume_path).split('.nii')[0]}_depth_x_{depth_x}.jpeg")
+    for depth_x in range(0, img_blurred.shape[0], args.save_every_n_cuts):
+        # Store ground truth image
+        im_gt = Image.fromarray(img_gt[depth_x, :, :])
+        im_gt.convert("L").save(os.path.join(
+            gt_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_x_{depth_x}.png")
         )
 
-    for depth_y in range(0, img.shape[1], args.save_every_n_cuts):
-        im = Image.fromarray(img[:, depth_y, :])
-        im.convert('RGB').save(os.path.join(
-            args.output_2D_images_dir,
-            f"{os.path.basename(args.volume_path).split('.nii')[0]}_depth_y_{depth_y}.jpeg")
+        # Store blurred image
+        im_blurred = Image.fromarray(img_blurred[depth_x, :, :])
+        im_blurred.convert("L").save(os.path.join(
+                blured_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_x_{depth_x}.png")
         )
 
-    for depth_z in range(0, img.shape[2], args.save_every_n_cuts):
-        im = Image.fromarray(img[:, :, depth_z])
-        im.convert('RGB').save(os.path.join(
-            args.output_2D_images_dir,
-            f"{os.path.basename(args.volume_path).split('.nii')[0]}_depth_z_{depth_z}.jpeg")
+    for depth_y in range(0, img_blurred.shape[1], args.save_every_n_cuts):
+        # Store ground truth image
+        im_gt = Image.fromarray(img_gt[:, depth_y, :])
+        im_gt.convert('L').save(os.path.join(
+            gt_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_y_{depth_y}.png")
+        )
+
+        # Store blurred image
+        im_blurred = Image.fromarray(img_blurred[:, depth_y, :])
+        im_blurred.convert('L').save(os.path.join(
+            blured_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_y_{depth_y}.png")
+        )
+
+    for depth_z in range(0, img_blurred.shape[2], args.save_every_n_cuts):
+        # Store ground truth image
+        im_gt = Image.fromarray(img_gt[:, :, depth_z])
+        im_gt.convert('L').save(os.path.join(
+            gt_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_z_{depth_z}.png")
+        )
+
+        # Store blurred image
+        im_blurred = Image.fromarray(img_blurred[:, :, depth_z])
+        im_blurred.convert('L').save(os.path.join(
+            blured_dir, f"{os.path.basename(args.blurred_volume_path).split('.nii')[0]}_depth_z_{depth_z}.png")
         )
