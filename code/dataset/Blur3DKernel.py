97c64634a1247631794792a7a2ed430f4c8959a7
JdbermeoUZH <juandiego.bermeoortiz@uzh.ch>
2022-12-29 09:42:12 -0500

    generate extra kernels for benchmarking

--- code/dataset/Blur3DKernel.py
+++ code/dataset/Blur3DKernel.py
@@ -1,9 +1,12 @@
 import os
-from typing import Tuple
+from typing import Tuple, Union
 
 import numpy as np
 from scipy.stats import multivariate_normal
+from scipy.ndimage import affine_transform, rotate, gaussian_filter
 import matplotlib.pyplot as plt
+
+from utils.common_utils import store_volume_nii_gz
 
 
 def _normalize_kernel(kernel: np.ndarray) -> np.ndarray:
@@ -27,10 +30,11 @@ class Blur3DKernel:
 
     def create_gaussian_kernel(
             self,
-            sigma_xyz: Tuple[float, float, float] = None,
-            center_xyz: Tuple[int, int, int] = (0, 0, 0),
+            sigma_xyz: Union[Tuple[float, float, float], np.ndarray] = None,
+            center_xyz: Tuple[float, float, float] = (0, 0, 0),
             plot_kernel: bool = False,
             persist_dir: str = None,
+            prefix_str: str = ''
     ) -> np.ndarray:
         x, y, z = self.xyz_mesh_grid
 
@@ -38,7 +42,7 @@ class Blur3DKernel:
         xyz = np.column_stack([x.flat, y.flat, z.flat])
         mu = np.array(center_xyz)
 
-        if sigma_xyz:
+        if sigma_xyz is not None:
             sigma_xyz = np.array(sigma_xyz)
         else:
             sigma_xyz = 0.3 * ((np.array(self.shape) - 1) * 0.5 - 1) + 0.8
@@ -54,8 +58,37 @@ class Blur3DKernel:
 
         if persist_dir:
             persist_path = os.path.join(
-                persist_dir, f"gaussian_sigmas_xyz_{'_'.join([str(sigma) for sigma in sigma_xyz])}_" +
+                persist_dir, f"{prefix_str}_gaussian_sigmas_xyz_{'_'.join([str(sigma) for sigma in sigma_xyz])}_" +
                              f"size_{'_'.join(test_kernel.shape.astype(str))}" + ".npy"
+            )
+
+            with open(persist_path, 'wb') as f:
+                np.save(f, kernel)
+                np.save(f, kernel)
+
+        return kernel
+
+    def create_tilted_gaussian(
+            self,
+            sigma_xyz: Union[Tuple[float, float, float], np.ndarray] = None,
+            center_xyz: Tuple[float, float, float] = (0, 0, 0),
+            plot_kernel: bool = False,
+            persist_dir: str = None,
+            prefix_str: str = ''
+    ):
+        x, y, z = self.xyz_mesh_grid
+        gaussian_kernel = self.create_gaussian_kernel(sigma_xyz, center_xyz)
+        kernel = rotate(gaussian_kernel, 45, axes=(2, 0), reshape=False)
+        kernel = rotate(kernel, 45, axes=(2, 1), reshape=False)
+        kernel /= kernel.sum()
+
+        # Plot the kernel
+        if plot_kernel:
+            plot_kernel_log_scale(kernel, x, y, z)
+
+        if persist_dir:
+            persist_path = os.path.join(
+                persist_dir, f"tilted_gaussian_sigmas_xyz_size_{'_'.join(test_kernel.shape.astype(str))}" + ".npy"
             )
 
             with open(persist_path, 'wb') as f:
@@ -105,21 +138,82 @@ class Blur3DKernel:
 
 
 if __name__ == "__main__":
+    kernels = {}
     kernel_dir = os.path.join("..", "..", "..", "data", "kernels")
     os.makedirs(kernel_dir, exist_ok=True)
 
-    # Gaussian kernel, usually dim = 3*sigma or 2*np.ceil(2*sigma + 1)
+    # Regular Gaussian kernel, usually dim = 3*sigma or 2*np.ceil(2*sigma + 1)
     kernel_size = (5, 5, 10)
-    sigma_xyz_ = (0.75, 0.75, 2.0)
     test_kernel = Blur3DKernel(kernel_dims_xyz=kernel_size)
-    #gaussian_kernel = test_kernel.create_gaussian_kernel(
-    #    plot_kernel=True,
-    #    sigma_xyz=None,#sigma_xyz_,
-    #    persist_dir=kernel_dir
-    #)
+
+    regular_gaussian_kernel = test_kernel.create_gaussian_kernel(
+        plot_kernel=True,
+        sigma_xyz=None,
+        persist_dir=kernel_dir,
+        prefix_str='regular'
+    )
+    kernels['regular_gaussian_kernel'] = regular_gaussian_kernel
+
+    # Displaced gaussian kernel
+    disp_gaussian_kernel = test_kernel.create_gaussian_kernel(
+        plot_kernel=True,
+        center_xyz=(0.5, 0.5, 2),
+        sigma_xyz=(0.85, 0.85, 1.4),  # sigma_xyz_,
+        persist_dir=kernel_dir,
+        prefix_str='displaced'
+    )
+    kernels['displaced_gaussian_kernel'] = disp_gaussian_kernel
+
+    # Broader gaussian Kernel
+    broader_gaussian_kernel = test_kernel.create_gaussian_kernel(
+        plot_kernel=True,
+        sigma_xyz=(1.5, 1.5, 2.25),
+        persist_dir=kernel_dir,
+        prefix_str='broader'
+    )
+    kernels['broader_gaussian_kernel'] = broader_gaussian_kernel
+
+    # tilted gaussian, 3*sigma or 2*np.ceil(2*sigma + 1)
+    sigma_xyz = 0.3 * ((np.array((3, 3, 6.5)) - 1) * 0.5 - 1) + 0.8
+    kernels['tilted_gaussian_kernel'] = test_kernel.create_tilted_gaussian(
+        plot_kernel=True,
+        sigma_xyz=sigma_xyz,
+        persist_dir=kernel_dir,
+        prefix_str='tilted'
+    )
+
+    # larger kernel
+    larger_kernel = Blur3DKernel(kernel_dims_xyz=(10, 10, 20))
+    broader_gaussian_kernel = larger_kernel.create_gaussian_kernel(
+        plot_kernel=True,
+        sigma_xyz=None,
+        persist_dir=kernel_dir,
+        prefix_str='larger'
+    )
+    kernels['broader_gaussian_kernel'] = broader_gaussian_kernel
 
     # Dumbbell kernel
     dumbbell_kernel = test_kernel.create_dumbbell_kernel_z(
         plot_kernel=True,
         persist_dir=kernel_dir
     )
+    kernels['dumbbell_kernel'] = dumbbell_kernel
+
+    os.makedirs('kernels', exist_ok=True)
+
+    for kernel_name, kernel_npy in kernels.items():
+        print(kernel_name)
+        store_volume_nii_gz(
+            vol_array=kernel_npy,
+            volume_filename=f'{kernel_name}.nii.gz',
+            output_dir='./kernels'
+        )
+
+        kernel_npy /= np.max(kernel_npy)
+
+        store_volume_nii_gz(
+            vol_array=(kernel_npy > 0.1).astype(np.uint8),
+            volume_filename=f'{kernel_name}_seg.nii.gz',
+            output_dir='./kernels'
+        )
+
